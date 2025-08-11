95c704f1db4c13d0a465cf9e703d26b1a08499b3
Pedro Campos <pdrocampos.santos@gmail.com>
2025-08-11 10:31:51 -0300

    feat: enviando modelo de predição e organização do agent

--- Agent/agent/agent.py
+++ Agent/agent/agent.py
@@ -2,6 +2,7 @@ from google.adk.agents import Agent
 from google.adk.tools import FunctionTool
 from tools.tool import predict_financing
 
+
 root_agent = Agent(
     model="gemini-2.0-flash",  # ou outro modelo Gemini que preferir
     name="financing_agent",
